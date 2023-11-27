414baefe48c1a2cda4f96790f8a0a40accb4e871
Stefan Zabrian <faniss2005@yahoo.com>
2023-11-27 21:14:42 +0200

    Up Candy Controller --REST get all api call

--- src/main/java/com/candyShop/rest/controller/CandyController.java
+++ src/main/java/com/candyShop/rest/controller/CandyController.java
@@ -1,10 +1,16 @@
 package com.candyShop.rest.controller;
 
+import com.candyShop.rest.model.Candy;
 import com.candyShop.rest.service.CandyService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @CrossOrigin
 @RestController
@@ -15,5 +21,13 @@ public class CandyController {
     @Autowired
     public CandyController(CandyService candyService) {
         this.candyService = candyService;
+    }
+    @GetMapping("/allCandies")
+    public ResponseEntity<?> getAll() {
+        if(candyService.getAll().isEmpty()) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("No content");
+        }
+        List<Candy> candies = candyService.getAll();
+        return ResponseEntity.status(HttpStatus.OK).body(candies);
     }
 }

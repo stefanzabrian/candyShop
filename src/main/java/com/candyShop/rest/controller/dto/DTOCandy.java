d6664855c0d99cdd1fadcbff1d573b8cfa5f79e4
Stefan Zabrian <faniss2005@yahoo.com>
2023-11-27 20:58:40 +0200

    Created DTOCandy for Controller

---
+++ src/main/java/com/candyShop/rest/controller/dto/DTOCandy.java
@@ -0,0 +1,59 @@
+package com.candyShop.rest.controller.dto;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+public class DTOCandy {
+    @NotNull
+    private int id;
+    @NotNull
+    @NotBlank
+    private String name;
+    @NotNull
+    private int price;
+    @NotNull
+    @NotBlank
+    private String description;
+
+    public DTOCandy() {
+    }
+
+    public DTOCandy(int id, String name, int price, String description) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.description = description;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}

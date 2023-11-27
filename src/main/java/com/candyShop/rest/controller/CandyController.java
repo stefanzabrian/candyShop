03767bdef78e13d0fb83d95b3e4d1526081ac38d
Stefan Zabrian <faniss2005@yahoo.com>
2023-11-27 21:24:56 +0200

    Up Candy Controller REST delete api call

--- src/main/java/com/candyShop/rest/controller/CandyController.java
+++ src/main/java/com/candyShop/rest/controller/CandyController.java
@@ -1,5 +1,6 @@
 package com.candyShop.rest.controller;
 
+import com.candyShop.rest.controller.exception.ResourceNotFoundException;
 import com.candyShop.rest.model.Candy;
 import com.candyShop.rest.service.CandyService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,14 +20,16 @@ public class CandyController {
     public CandyController(CandyService candyService) {
         this.candyService = candyService;
     }
+
     @GetMapping("/allCandies")
     public ResponseEntity<?> getAll() {
-        if(candyService.getAll().isEmpty()) {
+        if (candyService.getAll().isEmpty()) {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("No content");
         }
         List<Candy> candies = candyService.getAll();
         return ResponseEntity.status(HttpStatus.OK).body(candies);
     }
+
     @GetMapping("/candy/{id}")
     public ResponseEntity<?> getById(@PathVariable(value = "id") int id) {
         if (candyService.findById(id).isEmpty()) {
@@ -34,5 +37,16 @@ public class CandyController {
         }
         Candy candy = candyService.findById(id).get();
         return ResponseEntity.ok(candy);
+    }
+
+    @DeleteMapping("/candy/delete/{id}")
+    @ResponseStatus(value = HttpStatus.ACCEPTED)
+    public ResponseEntity<?> delete(@PathVariable(value = "id") int id) {
+        try {
+            candyService.delete(id);
+        } catch (ResourceNotFoundException e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+        }
+        return ResponseEntity.status(HttpStatus.ACCEPTED).body("Candy with deleted with id: " + id);
     }
 }

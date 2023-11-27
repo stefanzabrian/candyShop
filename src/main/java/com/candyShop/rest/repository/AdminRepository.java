26e71ecfd53724b99d65be0db8d8e1db6c56ccff
Stefan Zabrian <faniss2005@yahoo.com>
2023-11-28 01:41:40 +0200

    Created Admin Service Impl

--- src/main/java/com/candyShop/rest/repository/AdminRepository.java
+++ src/main/java/com/candyShop/rest/repository/AdminRepository.java
@@ -3,5 +3,8 @@ package com.candyShop.rest.repository;
 import com.candyShop.rest.model.Admin;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface AdminRepository extends JpaRepository<Admin,Integer> {
+    Optional<Admin> findByEmail(String email);
 }
--- src/main/java/com/candyShop/rest/service/AdminService.java
+++ src/main/java/com/candyShop/rest/service/AdminService.java
@@ -8,6 +8,8 @@ import java.util.Optional;
 public interface AdminService extends UserDetailsService {
     void save(String email,
               String password);
+    void saveNo(String email,
+              String password);
 
     Optional<Admin> findByEmail(String email);
 }

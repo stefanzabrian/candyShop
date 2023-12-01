a160d7cd3fc7d8d3b0490e7c082205abd8545a36
Stefan Zabrian <faniss2005@yahoo.com>
2023-12-01 13:54:32 +0200

    Created User Repository

--- src/main/java/com/candyShop/rest/model/User.java
+++ src/main/java/com/candyShop/rest/model/User.java
@@ -2,7 +2,6 @@ package com.candyShop.rest.model;
 
 import com.candyShop.rest.model.constant.UserRole;
 import jakarta.persistence.*;
-import org.springframework.stereotype.Component;
 
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;

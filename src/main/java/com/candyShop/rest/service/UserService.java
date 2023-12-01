624df222441889a99858a818d010a636ef18fcf9
Stefan Zabrian <faniss2005@yahoo.com>
2023-12-01 13:57:12 +0200

    Created UserService interface

---
+++ src/main/java/com/candyShop/rest/service/UserService.java
@@ -0,0 +1,25 @@
+package com.candyShop.rest.service;
+
+import com.candyShop.rest.controller.exception.ResourceNotFoundException;
+import com.candyShop.rest.model.User;
+import com.candyShop.rest.model.constant.UserRole;
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface UserService extends UserDetailsService {
+    Optional<User> findByEmail(String email);
+    List<User> getAll();
+    Optional<User> findById(int id);
+    void delete(int id) throws ResourceNotFoundException;
+    User create(
+            String email,
+            String password,
+            UserRole role
+    );
+    User update(
+            String email,
+            String password
+    ) throws ResourceNotFoundException;
+}

939e9276651a36ac389c2d34437fdef74d5fe95f
Stefan Zabrian <faniss2005@yahoo.com>
2023-12-01 13:50:06 +0200

    Created User Model

---
+++ src/main/java/com/candyShop/rest/model/User.java
@@ -0,0 +1,68 @@
+package com.candyShop.rest.model;
+
+import com.candyShop.rest.model.constant.UserRole;
+import jakarta.persistence.*;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+@Entity
+@Table(name = "user")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Integer id;
+    @Column(name = "email")
+    @NotNull
+    @NotBlank
+    @Email
+    private String email;
+    @NotNull
+    @NotBlank
+    private String password;
+    @Enumerated(EnumType.STRING)
+    private UserRole role;
+
+    public User() {
+    }
+
+    public User(String email, String password, UserRole role) {
+        this.email = email;
+        this.password = password;
+        this.role = role;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public UserRole getRole() {
+        return role;
+    }
+
+    public void setRole(UserRole role) {
+        this.role = role;
+    }
+}

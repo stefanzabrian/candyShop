331dab01c1097b1645d5b06cd7a89aa05ae48ee8
Stefan Zabrian <faniss2005@yahoo.com>
2023-12-02 18:09:37 +0200

    Created Roles Entity

---
+++ src/main/java/com/candyShop/rest/model/Roles.java
@@ -0,0 +1,37 @@
+package com.candyShop.rest.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "roles")
+public class Roles {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Integer id;
+    @Column(name = "name")
+    private String name;
+
+    public Roles() {
+    }
+
+    public Roles(String name) {
+        this.name = name;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}

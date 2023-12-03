8bbdcd7b8cfd33541cf41eb83af6c0f2380b2b4b
Stefan Zabrian <faniss2005@yahoo.com>
2023-12-03 16:27:11 +0200

    Update AuthResponseDto

--- src/main/java/com/candyShop/rest/controller/dto/AuthResponseDto.java
+++ src/main/java/com/candyShop/rest/controller/dto/AuthResponseDto.java
@@ -1,2 +1,29 @@
-package com.candyShop.rest.controller.dto;public class AuthResponseDto {
+package com.candyShop.rest.controller.dto;
+
+public class AuthResponseDto {
+    private String accessToken;
+    private String tokenType;
+
+    public AuthResponseDto() {
+    }
+
+    public AuthResponseDto(String accessToken) {
+        this.accessToken = accessToken;
+    }
+
+    public String getAccessToken() {
+        return accessToken;
+    }
+
+    public void setAccessToken(String accessToken) {
+        this.accessToken = accessToken;
+    }
+
+    public String getTokenType() {
+        return tokenType;
+    }
+
+    public void setTokenType(String tokenType) {
+        this.tokenType = tokenType;
+    }
 }

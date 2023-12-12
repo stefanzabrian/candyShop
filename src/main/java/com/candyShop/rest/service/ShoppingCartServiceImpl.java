86968adfd6b88ac7c82ca1c1138dd29eb75f0668
Stefan Zabrian <faniss2005@yahoo.com>
2023-12-12 20:51:25 +0200

    Updated ShoppingCartServiceImpl

--- src/main/java/com/candyShop/rest/service/ShoppingCartServiceImpl.java
+++ src/main/java/com/candyShop/rest/service/ShoppingCartServiceImpl.java
@@ -1,24 +1,49 @@
 package com.candyShop.rest.service;
 
 import com.candyShop.rest.model.Candy;
+import com.candyShop.rest.repository.OrderCandyRepository;
+import com.candyShop.rest.repository.OrderRepository;
 import org.springframework.context.annotation.Scope;
 import org.springframework.context.annotation.ScopedProxyMode;
 import org.springframework.stereotype.Service;
 import org.springframework.web.context.WebApplicationContext;
 
+import java.util.LinkedHashMap;
 import java.util.Map;
 
 @Service
 @Scope(value = WebApplicationContext.SCOPE_SESSION, proxyMode = ScopedProxyMode.TARGET_CLASS)
-public class ShoppingCartServiceImpl implements ShoppingCartService{
+public class ShoppingCartServiceImpl implements ShoppingCartService {
+    private static final String ORDER_PREFIX = "CANDYORD- ";
+    private final Map<Candy, Integer> cart = new LinkedHashMap<>();
+    private final UserService userService;
+    private final OrderCandyRepository orderCandyRepository;
+    private final OrderRepository orderRepository;
+
+    public ShoppingCartServiceImpl(UserService userService, OrderCandyRepository orderCandyRepository, OrderRepository orderRepository) {
+        this.userService = userService;
+        this.orderCandyRepository = orderCandyRepository;
+        this.orderRepository = orderRepository;
+    }
+
     @Override
     public void addCandy(Candy candy) {
-
+        if (cart.containsKey(candy)) {
+            cart.put(candy, cart.get(candy) + 1);
+        } else {
+            cart.put(candy, 1);
+        }
     }
 
     @Override
     public void removeCandy(Candy candy) {
-
+        if (cart.containsKey(candy)) {
+            if (cart.get(candy) == 1) {
+                cart.remove(candy);
+            } else {
+                cart.put(candy, cart.get(candy) - 1);
+            }
+        }
     }
 
     @Override

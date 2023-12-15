eaac2d4f2ab67e496da1c8bcad5bcbb86ba7facd
Stefan Zabrian <faniss2005@yahoo.com>
2023-12-15 21:27:48 +0200

    Updaten Order Service

--- src/main/java/com/candyShop/rest/service/OrderService.java
+++ src/main/java/com/candyShop/rest/service/OrderService.java
@@ -3,7 +3,6 @@ package com.candyShop.rest.service;
 import com.candyShop.rest.controller.exception.ResourceNotFoundException;
 import com.candyShop.rest.model.Client;
 import com.candyShop.rest.model.Order;
-import com.candyShop.rest.model.constant.OrderStatus;
 
 import java.util.Date;
 import java.util.List;
@@ -15,6 +14,5 @@ public interface OrderService {
     Optional<Order> findByIdAndClient(Integer id, Client client);
     List<Order> getAll();
     Optional<Order> findById(Integer id);
-    void update(Order order) throws ResourceNotFoundException;
     void delete(int id) throws ResourceNotFoundException;
 }
